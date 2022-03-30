e53fda0c6389c572658008c3bd9bcc055b37d847
AmrAwny <amrawnykhalil@gmail.com>
2022-03-31 01:31:34 +0200

    Added the First Code to the First Session

--- C Module/Sessions/Topic 1 Introduction RT/session1ex1/main.c
+++ C Module/Sessions/Topic 1 Introduction RT/session1ex1/main.c
@@ -3,11 +3,28 @@
 
 int main()
 {
-    int x = 3;
-    int y = 4;
-    int z;
-    z = x + y;
-    printf("z=%d", z);
+    char a[20];
+    char y[4];
+    char z[30];
+    int b;
+    int x;
+    float t;
+    long c;
+    printf("please enter your first name : ");
+    scanf("%s",&a);
+    printf("Hello MR. %s , can you add your phone number please ? ",a);
+    scanf("%ld",&c);
+    printf("oki,How old are you ? ");
+    scanf("%d",&x);
+    printf("oki, nice \n");
+    printf("Did you work in such field before? \n");
+    scanf("%s",&y);
+    printf("if yes ,tell us how many years of experience if no say  0 \n");
+    printf("years of experience : ");
+    scanf("%f",&t);
+    printf("what's the expected salary did you think : ");
+    scanf("%d",&b);
+    printf("thank you mr.%s we will contact with you in the nearst time",a);
 
-return 0;
+    return 0;
 }
